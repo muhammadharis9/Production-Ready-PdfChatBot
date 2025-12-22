81f5231370b042d685ee28ab2652359dd2af8d21
muhammadharis <muhammadharis.imtiaz1@gmail.com>
2025-12-22 04:56:23 +0100

    Project Structure Created!

---
+++ template.sh
@@ -0,0 +1,26 @@
+mkdir -p app
+mkdir -p app/core
+mkdir -p app/api
+mkdir -p app/api/v1
+mkdir -p app/schemas
+mkdir -p app/services
+
+touch .env
+touch .gitignore
+touch Dockerfile
+touch docker-compose.yml
+
+touch app/__init__.py
+touch app/main.py
+
+touch app/api/__init__.py
+touch app/api/v1/__init__.py
+touch app/api/v1/endpoints.py
+
+touch app/core/__init__.py
+touch app/core/config.py
+touch app/core/database.py
+
+touch app/schemas/__init__.py
+touch app/schemas/ingestion.py
+touch app/schemas/chat.py
