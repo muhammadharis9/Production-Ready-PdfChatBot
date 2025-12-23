650edb1b45a99446bc94c644bae69d1ff32b8da8
muhammadharis <muhammadharis.imtiaz1@gmail.com>
2025-12-23 01:09:58 +0100

    ingestion pipeline creation done

---
+++ app/schemas/ingestion.py
@@ -0,0 +1,64 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.core.database import get_vector_store
+import shutil
+
+def load_pdf(file_path):
+    loader = PyPDFLoader(file_path=file_path)
+    load = loader.load()
+    return load
+
+def splitting(load):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 800,
+        chunk_overlap= 100)
+
+    splitted_text = text_splitter.split_documents(load)
+    return splitted_text
+
+def process_pdf(file_path: str, session_id: str):
+ 
+    documents = load_pdf(file_path)
+    text_chunks = splitting(documents)
+    
+    vector_store = get_vector_store(session_id)
+
+    vector_store.add_documents(text_chunks)
+    
+    return {
+        "status": "Success", 
+        "filename": os.path.basename(file_path),
+        "chunks_processed": len(text_chunks)
+    }
+
+
+def embeds(splitted_text):
+    embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
+    embed = HuggingFaceEmbeddings(model=embedding_model)
+    PERSIST_DIR = "./chroma_db"
+    final_embed = Chroma.from_documents(
+        documents=splitted_text,
+        embedding=embed,
+        collection_name="pdfs",
+        persist_directory=PERSIST_DIR)
+    
+    return final_embed
+    
+
+#If you want to load your own model then use the following function
+ 
+def custom_embeds(load):
+    embedding_model1 = input("Please provide the model for embedding which you want to load: ")
+    embed = HuggingFaceEmbeddings(model=embedding_model)
+    
+    final_embed = Chroma.from_documents(
+        documents=splitted_text,
+        embedding=embed,
+        collection_name="pdfs",
+        persist_directory=PERSIST_DIR
+        )
+
+    return final_embed
